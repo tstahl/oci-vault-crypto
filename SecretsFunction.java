f28d5b2d96a524ce8bac61fbb32e624556e2e02d
Ty Stahl <ty.m.stahl@gmail.com>
2022-02-07 11:31:34 -0500

    Update and rename WalletSecretFunction.java to SecretsFunction.java

--- SecretsFunction.java
+++ SecretsFunction.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class WalletSecretFunction{
+public class SecretsFunction{
 
     private static String dbPassword;
     private static SecretsClient secretsClient;
